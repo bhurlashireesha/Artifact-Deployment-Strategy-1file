8b1c53d23597308a95b97a03b48e563bb7fdcd33
bhurlashireesha <81414778+bhurlashireesha@users.noreply.github.com>
2026-08-06 18:31:32 +0530

    one

--- scripts/delta.sh
+++ scripts/delta.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -e
+
 mkdir -p delta
-sf sgd source delta --to "$1" --from "$2" --output delta
+
+# Use --output-dir instead of --output
+sf sgd source delta --to "$1" --from "$2" --output-dir delta
